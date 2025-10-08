23f02c264385d7d351863488cdfd8f1539e2bc62
unknown <alicesisi.lin@gmail.com>
2025-10-08 11:24:09 +0200

    Modification exercice 1 cours 3

--- Devoirs/Exercice2/comptes.sh
+++ Devoirs/Exercice2/comptes.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+# Exercice 1 
+# Compte nombre de lignes contenant "Location"
+# Sur les fichiers ".ann" de l'exercice 1
+
 echo "Nombre de lieux en 2016"
 cat cat 2016/*/*.ann | grep Location | wc -l
 echo "Nombre de lieux en 201*"
