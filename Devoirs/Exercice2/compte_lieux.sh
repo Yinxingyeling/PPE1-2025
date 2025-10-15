0982470d8f8f760d1fbcc48ac003e7cb79896248
unknown <alicesisi.lin@gmail.com>
2025-10-15 11:49:56 +0200

    Correction d'exercices

--- Devoirs/Exercice2/compte_lieux.sh
+++ Devoirs/Exercice2/compte_lieux.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# DATADIR=$1 # Correction : ajout de la direction si besoin
 ANNEE=$1
 MOIS=$2
 NBR_LIEUX=$3
 
+# if [$# -ne 3]
+# then
+#     echo "Il faut 3 arguments" 
+#     exit
+# fi 
+
 # Test 1
-# cat "$ANNEE"/"$MOIS"/*.ann | grep Location | sort -k3 | uniq | cut -f3 | head -n"$NBR_LIEUX"
+cat "$ANNEE"/"$MOIS"/*.ann | grep Location | sort -k3 | uniq | cut -f3 | head -n"$NBR_LIEUX"
 
 # Amélioration finale
-cat "$ANNEE"/"$MOIS"/*.ann | grep Location | cut -f3 | sort -k3 | uniq -c | sort -k1 -r | head -n"$NBR_LIEUX"
+# cat "$DATADIR"/"$ANNEE"/"$MOIS"/*.ann | grep Location | cut -f3 | sort -k3 | uniq -c | sort -k1 -r | head -n"$NBR_LIEUX"
+
--- Devoirs/Exercice2/comptes.sh
+++ Devoirs/Exercice2/comptes.sh
@@ -5,8 +5,8 @@
 # Sur les fichiers ".ann" de l'exercice 1
 
 echo "Nombre de lieux en 2016"
-cat cat 2016/*/*.ann | grep Location | wc -l
+cat 2016/*/*.ann | grep Location | wc -l # --correction : suppression d'un cat en plus 
 echo "Nombre de lieux en 201*"
-cat cat 2017/*/*.ann | grep Location | wc -l
+cat 2017/*/*.ann | grep Location | wc -l 
 echo "Nombre de lieux en 2018"
-cat cat 2018/*/*.ann | grep Location | wc -l
+cat 2018/*/*.ann | grep Location | wc -l 
